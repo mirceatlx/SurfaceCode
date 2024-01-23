902cc64f15961d32710393bf66b1ff03721a8a39
MiniGareth <garethkit@gmail.com>
2024-01-23 23:47:19 +0100

    Added some tests

--- test.py
+++ test.py
@@ -29,5 +29,30 @@ def surface_code_five_cycles():
     for i in measurements[0].split(" "):
         assert i == measurements[0].split(" ")[0]
 
+def heavy_hex_lattice_direct_mapping_surface_code_five_cycles():
+    lattice = HeavyHexLattice(5)
+    square = DirectMap(lattice)
+    num_nodes = len(square.nodes)
+    surfacecode = SurfaceCodeCycle(square)
+
+    qc = surfacecode._circuit(5)
+    qc.add_register(ClassicalRegister(num_nodes))
+    qc.append(surfacecode._circuit(), range(num_nodes), range(5*num_nodes, 6* num_nodes))
+
+    aersim = AerSimulator(method="stabilizer")
+    built = transpile(qc, aersim)
+    job = aersim.run(built, shots = 1)
+    result = job.result()
+
+    measurements = list(result.get_counts().keys())
+
+    assert len(measurements) == 1
+    
+    for i in measurements[0].split(" "):
+        assert i == measurements[0].split(" ")[0]
+        
+
 if __name__ == "__main__":
     surface_code_five_cycles()
+    heavy_hex_lattice_direct_mapping_surface_code_five_cycles()
+
